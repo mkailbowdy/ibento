c6ea67c062531c64d7f3dcaee1a43115093b22d4
Myhkail Mendoza <myhkail.mendoza@gmail.com>
2024-03-09 10:08:23 +0900

    first commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+
+<title>My Blog</title>
+<head>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+<article>
+    <h1><a href="/posts/my-first-post">My First Post</a></h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae doloribus iure obcaecati officia
+        quibusdam quisquam? Architecto, culpa deserunt laboriosam laborum mollitia natus obcaecati rerum sed, sunt vel
+        vero voluptatem.</p>
+</article>
+<article>
+    <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae doloribus iure obcaecati officia
+        quibusdam quisquam? Architecto, culpa deserunt laboriosam laborum mollitia natus obcaecati rerum sed, sunt vel
+        vero voluptatem.</p>
+</article>
+<article>
+    <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores beatae doloribus iure obcaecati officia
+        quibusdam quisquam? Architecto, culpa deserunt laboriosam laborum mollitia natus obcaecati rerum sed, sunt vel
+        vero voluptatem.</p>
+</article>
+</body>
